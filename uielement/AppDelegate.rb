8df67368a48c2589f0a71abd49042de15aeac94e
Takahashi Hiroyuki <hiro@Local-no-MacBook-Pro.local>
2011-10-03 13:40:20 +0900

    Initial Commit

---
+++ uielement/AppDelegate.rb
@@ -0,0 +1,15 @@
+#
+#  AppDelegate.rb
+#  uielement
+#
+#  Created by Hiroyuki Takahashi on 11/10/03.
+#  Copyright 2011年 __MyCompanyName__. All rights reserved.
+#
+
+class AppDelegate
+  attr_accessor :window
+  def applicationDidFinishLaunching(a_notification)
+    # Insert code here to initialize your application
+  end
+end
+
