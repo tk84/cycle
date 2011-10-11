318cee497a3fa081881288db402743c0d286b596
Takahashi Hiroyuki <hiro@tk84pro.local>
2011-10-12 05:32:06 +0900

    delete uielement

--- uielement/ScriptingInterface.rb
+++
@@ -1,9 +0,0 @@
-#
-#  ScriptingInterface.rb
-#  uielement
-#
-#  Created by Hiroyuki Takahashi on 11/10/03.
-#  Copyright 2011年 __MyCompanyName__. All rights reserved.
-#
-
-
--- uielement/WindowServer.rb
+++
@@ -1,9 +0,0 @@
-#
-#  WindowServer.rb
-#  uielement
-#
-#  Created by Hiroyuki Takahashi on 11/10/04.
-#  Copyright 2011年 __MyCompanyName__. All rights reserved.
-#
-
-
